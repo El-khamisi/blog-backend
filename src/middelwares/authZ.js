c751926fdab72402eedc3e2867140f5d6a4ce3f6
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-06-08 14:56:21 +0200

    writers

--- src/middelwares/authZ.js
+++ src/middelwares/authZ.js
@@ -7,9 +7,13 @@ const { TOKENKEY, NODE_ENV } = require('../config/env');
 
 exports.isAdmin = (req, res, next) => {
   try {
-    const role = res.locals.user.role;
-    if (role && role == Admin) return next();
-    else throw new Error('You are NOT authorized to Admins Only Routes');
+    if(res.locals.user){
+      const role = res.locals.user.role;
+      if (role && role == Admin) return next();
+      else throw new Error('You are NOT authorized to Admin Routes');
+    }else{
+      throw new Error('Login first');
+    }
   } catch (e) {
     if (e instanceof ReferenceError) return failedRes(res, 500, e);
     else return failedRes(res, 401, e);
