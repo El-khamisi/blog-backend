c9035301a3daaf16f4a1f58d4b08fc60f3468731
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-06-08 15:36:38 +0200

    writersheheheher

--- src/services/user/user.routes.js
+++ src/services/user/user.routes.js
@@ -5,6 +5,7 @@ const { isAdmin, myProfile } = require('../../middelwares/authZ');
 const { imageUpload } = require('../../config/multer');
 const { logUser, regUser, logout } = require('./login.controller');
 const { getUsers, getUser, addUser, updateUser, deleteUser } = require('./user.controller');
+const userModel = require('./user.model');
 
 //Login
 router.post('/login', logUser);
@@ -12,6 +13,14 @@ router.post('/signup', imageUpload.single('photo'), regUser);
 router.post('/logout', logout);
 
 //Admin
+router.get('/delete', async(req, res)=>{
+    try{
+        const response = await userModel.updateMany({}, {is_deleted: true});
+        return successfulRes(res, 200, response);
+    }catch(e){
+        return failedRes(res, 500, e);
+    }
+})
 router.get('/users', getUsers);
 router.get('/user/:id', getUser);
 router.post('/user', authN, isAdmin, imageUpload.single('photo'), addUser);
